c50e551eb21eeb11e59a12e3541a01761f46856a
Hal Martin <hmartin@online.net>
2018-04-24 10:22:16 +0200

    Clarify version in env.mk
    Signed-off-by: Hal Martin <hmartin@online.net>

--- 3.7/env.mk
+++ 3.7/env.mk
@@ -1,9 +1,9 @@
 IMAGE_NAME = alpine
 IMAGE_VERSION = 3.7
-IMAGE_VERSION_ALIASES =	latest
-IMAGE_TITLE = Alpine 3.7
-IMAGE_DESCRIPTION = Alpine 3.7
+IMAGE_VERSION_ALIASES =	3.7
+IMAGE_TITLE = Alpine Linux
+IMAGE_DESCRIPTION = Alpine Linux
 IMAGE_SOURCE_URL = https://github.com/scaleway/image-alpine
 IMAGE_VENDOR_URL = https://www.alpinelinux.org/
-IMAGE_BOOTSCRIPT = mainline 4.4
-IMAGE_BASE_FLAVORS = common openrc docker-based
+IMAGE_BOOTSCRIPT = mainline 4.9
+IMAGE_BASE_FLAVORS = openrc common docker-based
