79b7f3010c44f329421d1104146a48c946a1a729
Jérôme Tamba <jtamba@online.net>
2019-08-30 17:38:49 +0200

    Update bootscript to 4.19 latest-tracking

--- latest/env.mk
+++ latest/env.mk
@@ -4,5 +4,5 @@ IMAGE_TITLE = Alpine Linux
 IMAGE_DESCRIPTION = Alpine Linux
 IMAGE_SOURCE_URL = https://github.com/scaleway/image-alpine
 IMAGE_VENDOR_URL = https://www.alpinelinux.org/
-IMAGE_BOOTSCRIPT = mainline 4.14
+IMAGE_BOOTSCRIPT = longterm 4.19 latest
 IMAGE_BASE_FLAVORS = openrc common docker-based
